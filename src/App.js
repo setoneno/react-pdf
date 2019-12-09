a2c7de0e2731150774c03fdcbbc71ab413507b17
setoneno <setone0222@gmail.com>
2019-12-09 17:12:13 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,68 +3,91 @@ import { Document, Page, pdfjs } from "react-pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
-export default class App extends Component {
-    render() {
-      return (
-        <div>
-          <Body />
-        </div>
-      );
-    }
-  }
-
- class Body extends Component {
-    constructor(props) {
-        super(props);
-  this.state = { numPages: null, pageNumber: 1 };
-}
-onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
-//   nextDocumentLoadSuccess = ({ numPages }) => {
-//     this.setState({ numPages });
-//   };
-
-//   componentWillMount() {
-//     setTimeout(() => {
-//       this.setState({
-//          numPages: null, pageNumber: 1
-//       });
-//     }, 2000);
+// export default class App extends Component {
+//     render() {
+//       return (
+//         <div>
+//           <Body />
+//         </div>
+//       );
+//     }
 //   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      const { pageNumber, numPages } = this.state;
-      this.setState(state => ({ pageNumber: state.pageNumber }, 
-      { value: (
-      <div style={{ width: 600 }}>
-        <Document file="/linux.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
-          <Page pageNumber={pageNumber} width={600} />
-        </Document>
-      </div>) }));
-    }, 2000);
-  }
+export default class App extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { numPages: null, pageNumber: 1 };
+    //   }
+  state = { numPages: null, pageNumber: 1 };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+      this.setState({ numPages });
+    };
+
+  nextDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+   };
 
   goToPrevPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
   goToNextPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
 
-  render() {
-    const { pageNumber, numPages } = this.state;
+//   nextDocumentLoadSuccess = ({ numPages }) => {
+//     this.setState({ numPages });
+//   };
 
-    return (
-      <div>
+
+  componentWillMount() {
+    setTimeout(() => {
+      const { pageNumber, numPages } = this.state;
+      this.setState(state => ({ pageNumber: state.pageNumber }, 
+      { value: (
+        <div>
         <nav>
           <button onClick={this.goToPrevPage}>Prev</button>
           <button onClick={this.goToNextPage}>Next</button>
         </nav>
-         {this.state.value}
-        <p>
+      <div style={{ width: 600 }}>
+        <Document file="/React.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
+          <Page pageNumber={pageNumber} width={600} />
+        </Document>
+      </div>
+      <p>
           Page {pageNumber} of {numPages}
-        </p>
+      </p>
+      </div>) }));
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const { pageNumber, numPages } = this.state;
+      this.setState(state => ({ pageNumber: state.pageNumber }, 
+      { value: (
+        <div>
+        <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+      <div style={{ width: 600 }}>
+        <Document file="/linux.pdf" onLoadSuccess={this.nextDocumentLoadSuccess} >
+          <Page pageNumber={pageNumber} width={600} />
+        </Document>
+      </div>
+      <p>
+          Page {pageNumber} of {numPages}
+      </p>
+      </div>) }));
+    },5000);
+  }
+
+  render() {
+    // const { pageNumber, numPages } = this.state;
+    
+    return (
+      <div>
+         {this.state.value}
       </div>
     );
   }
