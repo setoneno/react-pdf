9c77e123145ee44647bccb7648a5c8920b43389b
setoneno <setoneno@KeisukenoMacBook-Pro.local>
2020-06-22 19:30:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,133 +2,41 @@ import React, { Component } from "react";
 import { Document, Page, pdfjs } from "react-pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function ChangeButton(props) {
-	console.log("changenonaka")
-  return (
-    <a href={props.link} onClick={() => props.onClick()}>
-      {props.value}
-    </a>
-  );
-}
-
-
-
-let a = 0;
-console.log("letnoato")
-
 export default class App extends Component {
-      state = { numPages: null, pageNumber: 1 };
+  state = { numPages: null, pageNumber: 1 };
 
   onDocumentLoadSuccess = ({ numPages }) => {
-		this.setState({ numPages });
-		console.log("daunro-doowata")
-	};
-	
-
+    this.setState({ numPages });
+  };
 
   goToPrevPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
   goToNextPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
-		
-	
-			fff() {
-				const { pageNumber, numPages } = this.state;
-				this.setState(state => ({ pageNumber: state.pageNumber }, 
-				{ value: (
-					<div>
-				<div style={{ width: 600 }}>
-					<Document file="/React.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
-						<Page pageNumber={a } width={600} />
-					</Document>
-				</div>
-				<p>
-						Page {pageNumber} of {numPages}
-				</p>
-				</div>) }));
-			}
-
-  
-
-  first() {
-      const { pageNumber, numPages } = this.state;
-      this.setState(state => ({ pageNumber: state.pageNumber }, 
-      { value: (
-        <div>
-      <div style={{ width: 600 }}>
-        <Document file="/React.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
-          <Page pageNumber={a} width={600} />
-        </Document>
-      </div>
-      <p>
-          Page {pageNumber} of {numPages}
-      </p>
-      </div>) }));
-  }
-
-  second() {
-      const { pageNumber, numPages } = this.state;
-      this.setState(state => ({ pageNumber: state.pageNumber },
-      { value: (
-        <div>
-      <div style={{ width: 600 }}>
-        <Document file="/linux.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
-          <Page pageNumber={a} width={600} />
-        </Document>
-      </div>
-      <p>
-          Page {pageNumber} of {numPages}
-      </p>
-      </div>) }));
-  }
-
 
   render() {
-		console.log("renderato")
+    const { pageNumber, numPages } = this.state;
 
-		function Hoge(props) {
-			console.log(a);
-			a = a + 1;
-			console.log(a);
-			return (
-				<a href={props.link} onClick={() => props.onClick()}>
-					{props.value}
-				</a>
-			);
-		}
     return (
       <div>
         <nav>
           <button onClick={this.goToPrevPage}>Prev</button>
-          <Hoge onClick={() => this.fff()}/>
-				
+          <button onClick={this.goToNextPage}>Next</button>
         </nav>
-        <ul>
-					<li>
-					<Hoge link="#3" value="test" onClick={() => this.fff()} />
-					</li>
-          <li>
-            <ChangeButton link="/" value="main" onClick={() => this.state()} />
-          </li>
-          <li>
-            <ChangeButton link="#1" value="page1" onClick={() => this.first()} />
-          </li>
-          <li>
-            <ChangeButton link="#2" value="page2" onClick={() => this.second()} />
-          </li>
-        </ul>
-        {this.state.value}
+
+        <div style={{ width: 600 }}>
+          <Document
+            file="/React.pdf"
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
         </div>
+
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
     );
   }
 }
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Body />
-//       </div>
-//     );
-//   }
-// }
